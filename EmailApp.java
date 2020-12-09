30073f6e7c367937acbe5118cf6b046e26e7628c
Beatrix Woo <beatrixwho@gmail.com>
2020-12-08 16:14:00 -0800

    print displayInfo method

--- EmailApp.java
+++ EmailApp.java
@@ -3,7 +3,7 @@ package emailapp;
 public class EmailApp {
     public static void main(String[] args){
         Email em1 = new Email("John", "Smith");
-        em1.displayInfo();
+        System.out.println(em1.displayInfo());
 
         // em1.setAlternateEmail("beatrix@gmail.com");
         // System.out.println(em1.getAlternateEmail())
